b24883cebfe3d225cba66fbf0d199123c8d5601d
Marblexake <qazx29@gmail.com>
2019-08-15 17:12:09 +0800

    json tryout

---
+++ !scripts/limitless/jsonFile/VoiceOverLine.cs
@@ -0,0 +1,19 @@
+using System;
+
+[Serializable]
+public class VoiceOverLine
+{
+    public string key;
+    public string line;
+
+    // My own
+    /*
+    public float time;
+
+    public string subtitle;
+
+    public string trigger;
+
+    public string colour;
+     */
+}
